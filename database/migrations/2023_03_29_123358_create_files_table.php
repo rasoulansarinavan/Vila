f93479f7ca7ffeb51dc84a354f145bf0f6c05d52
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2023-03-31 13:03:15 +0430

    create files migration

---
+++ database/migrations/2023_03_29_123358_create_files_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->string('type')->default('image');
+            $table->foreignId('product_id')->nullable();
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->boolean('cover')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('files');
+    }
+};
