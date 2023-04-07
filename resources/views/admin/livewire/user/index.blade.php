d9b5174ca0750e1adc060448d7d86ed0ae871fca
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2023-04-07 18:09:52 +0430

    add admin user index in view

---
+++ resources/views/admin/livewire/user/index.blade.php
@@ -0,0 +1,17 @@
+<section class="content-main">
+    <div class="content-header">
+    </div>
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <livewire:admin.user.user-table/>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
